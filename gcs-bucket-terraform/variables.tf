b046e673f812de6fbc852c132a1d3a9a3f7c0052
Michael Butterfield <michaelrb13579@gmail.com>
2025-06-13 17:12:26 +0100

    Initialising terraform, building gcs bucket

---
+++ gcs-bucket-terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The region for the bucket"
+  type        = string
+  default     = "US"
+}
+
+variable "bucket_name" {
+  description = "The name of the storage bucket"
+  type        = string
+}
+
+variable "credentials_file" {
+  type        = string
+  description = "Path to service account JSON key"
+}
