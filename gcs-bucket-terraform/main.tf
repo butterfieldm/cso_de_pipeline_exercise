62f7d173390597a4f3951979d2cc143c0033eebf
Michael Butterfield <michaelrb13579@gmail.com>
2025-06-17 17:15:47 +0100

    Fix: IAM rules

--- gcs-bucket-terraform/main.tf
+++ gcs-bucket-terraform/main.tf
@@ -114,12 +114,6 @@ resource "google_pubsub_topic_iam_member" "allow_gcs_publish" {
   member = "serviceAccount:service-${data.google_project.project.number}@gs-project-accounts.iam.gserviceaccount.com"
 }
 
-resource "google_pubsub_topic_iam_member" "gcs_publish_permission" {
-  topic    = google_pubsub_topic.gcs_notifications.name
-  role     = "roles/pubsub.publisher"
-  member   = "serviceAccount:service-${data.google_project.project.number}@gs-project-accounts.iam.gserviceaccount.com"
-}
-
 resource "google_pubsub_topic_iam_member" "allow_gcs_publish" {
   topic = google_pubsub_topic.gcs_notifications.name
   role  = "roles/pubsub.publisher"
