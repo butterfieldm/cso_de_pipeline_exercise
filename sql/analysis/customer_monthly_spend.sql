61d0d8a9f34fff5a7030331fa23d25f149919a78
Michael Butterfield <michaelrb13579@gmail.com>
2025-06-26 16:52:22 +0100

    Fix: Updating parittion to cast as date

--- sql/analysis/customer_monthly_spend.sql
+++ sql/analysis/customer_monthly_spend.sql
@@ -4,7 +4,7 @@ CLUSTER BY customer_id AS
 SELECT
   a.customer_id,
   CONCAT(b.first_name, ' ', b.last_name) AS full_name,
-  DATE_TRUNC(a.transaction_dttm, MONTH) AS month_date, 
+  DATE_TRUNC(CAST(a.transaction_dttm AS DATE), MONTH) AS month_date,
   SUM(a.amount) AS total_monthly_spend,
   AVG(a.amount) AS avg_transaction_amount
 FROM `cso-deng-pipeline.cso_exercise_bq_curated.transactions` a
